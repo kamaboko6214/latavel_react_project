c474b34b480548776784e217c7582a80f07d7c15
かまぼこ <k.lusnm0512@gmail.com>
2023-03-26 18:30:08 +0900

    Laravel+Reactの認証機能に変更する認証機能に変更する

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -2,8 +2,6 @@
 import "./bootstrap";
 import ReactDOM from "react-dom/client";
 import Example from "./components/Example";
-import Register from "./components/Register";
-import Login from "./components/Login";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import GroupIndex from "./components/GroupIndex";
@@ -16,8 +14,6 @@ function App() {
                 <Header />
                     <Routes>
                         <Route path="/" element={<GroupIndex />}/>
-                        <Route path="/a" element={<Register />} />
-                        <Route path="/b" element={<Login/>} />
                     </Routes>
                 <Footer />
             </div>
