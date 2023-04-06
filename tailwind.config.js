cd4a47be66b9fa97168b2f5b7e5bd9dd90ed0554
かまぼこ <k.lusnm0512@gmail.com>
2023-04-06 23:17:50 +0900

    add:トップ画面の作成

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,8 +12,9 @@ module.exports = {
           // 'カラー名': 'カラーコード'
           'green-bg': '#88B168',
           'bg-content': '#E1C292',
+          'oriblue' : '#6FB3C1',
+          'hovercol': '#4ca0b1',
         },
-  
       },
   },
   plugins: [],
